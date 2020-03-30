7bc6692220462a54c999c5c480234bf5e77bf9fa
Francesco <francesco.ferruccio88@gmail.com>
2020-03-30 15:10:02 +0200

    eliminato prompt e aggiunto campo di testo come input utente

---
+++ palindromo/bonus/script/main.js
@@ -0,0 +1,32 @@
+// Chiedo a utente di inserire come input una parola
+// tramite una funzione (creata da te) verifico se la parola inserita è palindroma o meno;
+// dò output a utente dicendogli che ho verificato;
+// qui sicuramente da cercare ci sarà il come revertare una parola.
+
+var output = document.getElementById('output');
+var btnStart = document.getElementById('start');
+
+btnStart.addEventListener("click",
+  function() {
+    // salvo la parola inserita dall'utente
+    var parola = document.getElementById('parola').value;
+    // richiamo la funzione e do l'output in base al valore ritornato dalla funzione
+    if (palindromo(parola)) {
+      output.innerHTML = parola + " è una parola palindroma.";
+    } else {
+      output.innerHTML = parola + " NON è una parola palindroma.";
+    }
+  }
+)
+
+// creo una funzione che controlla se la parola che gli passiamo è palindroma
+ function palindromo (stringa) {
+   // trasformo la stringa in un array di caratteri, inverto l'array e riformo la stringa
+   var stringaReverse = stringa.split("").reverse().join("");
+   // controllo se la stringa invertita è uguale a quella iniziale e ritorno un valore booleano
+   if(stringa.toUpperCase() == stringaReverse.toUpperCase()) {
+     return true;
+   } else {
+     return false;
+   }
+ }
